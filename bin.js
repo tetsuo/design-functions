d22c801bd94ee8e2dd5207c2d70b1915f7442491
Onur Gunduz <ogunduz@gmail.com>
2020-08-24 23:07:06 +0200

    Init

---
+++ bin.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const resolve = require('./resolve')
+const createEntry = require('./entry')
+
+const main = cb => {
+  const { entries, babelify } = process.argv
+    .slice(2)
+    .reduce(
+      (acc, x) =>
+        Array.isArray(acc)
+          ? Object.assign(acc[1], { [acc[0]]: JSON.parse(x) })
+          : x === '--babelify'
+          ? [x.slice(2), acc]
+          : { entries: acc.entries.concat(path.resolve(x)) },
+      { entries: [], babelify: {} }
+    )
+
+  resolve(entries, babelify, (er, res) =>
+    cb(
+      er || null,
+      res &&
+        res
+          .map(d => createEntry(res, d, entries.indexOf(d.id) >= 0))
+          .reduce(
+            (acc, x) => {
+              if (x.type === 'Reduce') {
+                acc.views[x.name] = acc.views[x.name] || {}
+                acc.views[x.name].reduce = x.source
+              } else if (x.type === 'Map') {
+                acc.views[x.name] = acc.views[x.name] || {}
+                acc.views[x.name].map = x.source
+              } else if (x.type === 'Filter') {
+                acc.filters[x.name] = x.source
+              } else if (x.type === 'Update') {
+                acc.updates[x.name] = x.source
+              } else if (x.type === 'ValidateDocumentUpdate') {
+                acc.validate_doc_update = x.source
+              } else if (x.type === 'Lib') {
+                acc.views.lib[String(x.id)] = x.source
+              }
+              return acc
+            },
+            { views: { lib: {} }, updates: {}, filters: {} }
+          )
+    )
+  )
+}
+
+module.exports = main
+
+const exit = (code, msg) => {
+  if (msg) {
+    if (code === 0) {
+      console.log(msg)
+    } else {
+      console.error(msg)
+    }
+  }
+  process.exit(code)
+}
+
+if (!module.parent) {
+  main((er, res) => (er ? exit(1, er.message) : console.log(JSON.stringify(res, null, 2))))
+}
